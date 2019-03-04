7c29d4eb9386eba6ed73e9e92d48ed190194d32a
briancollins081 <briancollins081@gmail.com>
2019-03-05 01:31:03 +0300

    Added book update logic and formatted that pages

--- src/main/java/com/brian/store/adminportal/controller/BookController.java
+++ src/main/java/com/brian/store/adminportal/controller/BookController.java
@@ -3,6 +3,8 @@ package com.brian.store.adminportal.controller;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
@@ -41,7 +43,7 @@ public class BookController {
 		try {
 			byte[] bytes = bookImage.getBytes();
 			String name = book.getId() + ".jpg";
-			//String name = book.getId() + ".png";
+			// String name = book.getId() + ".png";
 			BufferedOutputStream stream = new BufferedOutputStream(
 					new FileOutputStream(new File("src/main/resources/static/img/book/" + name)));
 			stream.write(bytes);
@@ -52,26 +54,52 @@ public class BookController {
 		}
 		return "redirect:bookList";
 	}
-	
+
 	@RequestMapping("/bookInfo")
-	public String bookInfo(@RequestParam("id") Long id,Model model) {
-		 Book book=bookService.findOne(id);
-		 model.addAttribute("book", book);
+	public String bookInfo(@RequestParam("id") Long id, Model model) {
+		Book book = bookService.findOne(id);
+		model.addAttribute("book", book);
 		return "bookInfo";
 	}
-	
+
 	@RequestMapping("/updateBook")
-	public String updateBook(@RequestParam("id") Long id,Model model) {
-		 Book book=bookService.findOne(id);
-		 model.addAttribute("book", book);
-		 
+	public String updateBook(@RequestParam("id") Long id, Model model) {
+		Book book = bookService.findOne(id);
+		model.addAttribute("book", book);
+
 		return "updateBook";
 	}
-	
+
+	@RequestMapping(value = "/updateBook", method = RequestMethod.POST)
+	public String updateBookPost(@ModelAttribute("book") Book book, HttpServletRequest request) {
+		bookService.save(book);
+
+		MultipartFile bookImage = book.getBookImage();
+		if (!bookImage.isEmpty()) {
+			try {
+				byte[] bytes = bookImage.getBytes();
+				String name = book.getId() + ".jpg";
+				
+				Files.delete(Paths.get("src/main/resources/static/img/book/"+name));
+				
+				// String name = book.getId() + ".png";
+				BufferedOutputStream stream = new BufferedOutputStream(
+						new FileOutputStream(new File("src/main/resources/static/img/book/" + name)));
+				stream.write(bytes);
+				stream.close();
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+		return "redirect:/book/bookInfo?id="+book.getId();
+	}
+
 	@RequestMapping("/bookList")
 	public String bookList(Model model) {
-		 List<Book> bookList=bookService.findAll(); 
-		 model.addAttribute("bookList",bookList);
+		List<Book> bookList = bookService.findAll();
+		model.addAttribute("bookList", bookList);
 		return "bookList";
 	}
 }
